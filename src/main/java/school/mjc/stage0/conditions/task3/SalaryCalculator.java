33579b7658e6644dde4c140d475105fdc886c386
Masees85 <109299067+Masees85@users.noreply.github.com>
2022-08-09 09:01:43 -0400

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -1,6 +1,43 @@
 package school.mjc.stage0.conditions.task3;
 
-public class SalaryCalculator {
-    public void calculateSalary(int salary) {
+public class SalaryCalculator 
+
+{
+    public void calculateSalary(int salary) 
+    
+    {
+            double temp;
+            double salaryAfterTax;
+    
+    if (salary >0 && salary < 2147483647 )
+   
+    {
+        if(salary > 0 && salary < 10000)
+        {
+            temp = salary * 0.15;
+            salaryAfterTax= salary-temp;
+            System.out.println(salaryAfterTax);
+        }
+        
+        if(salary >= 10000 && salary < 20000)
+        {
+            temp = salary * 0.18;
+            salaryAfterTax= salary-temp;
+            System.out.println(salaryAfterTax);
+        }
+        
+        
+        if(salary >= 20000 )
+        {
+            temp = salary * 0.20;
+            salaryAfterTax= salary-temp;
+            System.out.println(salaryAfterTax);
+        }
+    }
+    
+    else
+    {
+        System.out.println("wrong input!");
+    }
     }
 }
