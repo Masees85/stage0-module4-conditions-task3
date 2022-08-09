d1bfa122babf817b21ef7b5658439e7ab68e44cb
Masees85 <109299067+Masees85@users.noreply.github.com>
2022-08-09 09:02:53 -0400

    Update Seasons.java

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -1,7 +1,41 @@
 package school.mjc.stage0.conditions.task3;
 
-public class Seasons {
-    public void tellTheSeasonByMonthNumber(int month) {
+public class Seasons 
+
+{
+    public void tellTheSeasonByMonthNumber(int month) 
+    
+    {
+        
+                if(month>=1 && month<=12)
+        {
+            
+              if (month>=6 && month<=8)
+        {
+            System.out.println("Summer");
+        }
+              if (month>=9 && month<=11)
+        {
+            System.out.println("Fall");
+        }
+        
+     
+        
+        if(month==12 || month==1 || month==2)
+        {
+            System.out.println("Winter");
+        }
+        
+        if(month>=3 && month<=5)
+        {
+            System.out.println("Spring");
+        }
+        }       
+                           
+        else
+        {
+            System.out.println("wrong number!");
+        }
 
     }
 }
